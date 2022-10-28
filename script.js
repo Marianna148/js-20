a407a675ba065d5b7597a3b2313fb89bc9d7390e
Marianna148 <99441179+Marianna148@users.noreply.github.com>
2022-10-28 15:32:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const root = document.querySelector('.root');
+
+const setClass = (level, element, newClass) => {
+     
+    if(typeof level =='number') level--
+    let ulList = [...list.children];
+    if(level == 'last') level = ulList.length-1
+    console.log(level);
+    ulList[level].classList.add(newClass);
+    ulList[level].classList.add('animation');
+};
+
+setTimeout(() => {
+    setClass(root, 1, 'first');
+    setClass(root, 'last', 'last')
+}, 2000);
